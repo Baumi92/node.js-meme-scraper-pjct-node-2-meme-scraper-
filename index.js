ca875c31ec41d2851c87ecde57bc35b930f59acf
Manuel Baumgartner <manu.baumi@outlook.com>
2023-04-27 19:42:58 +0200

    Add more Framework

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const url = 'https://memegen-link-examples-upleveled.netlify.app/';
 axios
   .get(url)
   .then((response) => {
+    // load the HTML response into Cheerio
     const $ = cheerio.load(response.data);
+    // get the first section element
     const section = $('section').first();
+    // get the first 10 img elements within the section
     const images = section.find('img').slice(0, 10);
 
     images.each((i, img) => {
+      // log the src attribute of each img element
       console.log($(img).attr('src'));
     });
   })
